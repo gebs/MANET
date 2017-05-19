78396778a4b175172890e1d5ad166289405648b0
psychonoob <adrian.gebs@stud.hslu.ch>
2017-05-19 23:59:42 +0200

    Anpassung auf max 10 Elemente in LIste

--- src/main/java/ch/hslu/mobsys/manet/MessageCache.java
+++ src/main/java/ch/hslu/mobsys/manet/MessageCache.java
@@ -7,16 +7,22 @@ import javafx.collections.ObservableList;
 
 @Singleton
 public class MessageCache {
+    private static final int DEFAULT_SIZE = 10;
 
     public ObservableList<ManetMessage> getManetMessageListProperty() {
         return manetMessageListProperty;
     }
 
+    private int size;
     private ObservableList<ManetMessage> manetMessageListProperty;
 
-    public MessageCache() {
-
+    public MessageCache(int size){
         manetMessageListProperty = FXCollections.observableArrayList();
+        this.size = size;
+    }
+
+    public MessageCache() {
+        this(DEFAULT_SIZE);
     }
 
     public ManetMessage getExisting(ManetMessage manetMessage) {
@@ -32,6 +38,9 @@ public class MessageCache {
     }
 
     public void cacheMessage(ManetMessage manetMessage) {
+        if (manetMessageListProperty.size() == size){
+            manetMessageListProperty.remove(0);
+        }
         manetMessageListProperty.add(manetMessage);
     }
 
